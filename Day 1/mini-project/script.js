290fef08e52aeb07cd004b97e35056259cd9a8bd
VABrocks <111216246+VABrocks@users.noreply.github.com>
2024-06-14 08:01:04 +0530

    Add files via upload

--- Day 1/mini-project/script.js
+++ Day 1/mini-project/script.js
@@ -1,20 +1,20 @@
-function startTask() {
-    let character = document.getElementById("character").value;
-    let task;
-    switch (character) {
-        case "abhishek":
-            task = "File an RTI";
-            break;
-        case "professor":
-            task = "Plan a heist";
-            break;
-        case "eleven":
-            task = "Close the Gate";
-            break;
-        default:
-            task = "Select a valid character";
-    }
-    document.getElementById("task").innerHTML = "Task: " + task;
-    document.getElementById("message").innerHTML = "Good luck!";
-    document.getElementById("message").classList.add("highlight");
+function add(a, b) {
+  c = document.getElementById("para1");
+  
+  c.innerHTML = (a + b);
+}
+function subtract(a, b) {
+  c = document.getElementById("para1");
+  
+  c.innerHTML = (a - b);
+}
+function multiply(a, b) {
+  c = document.getElementById("para1");
+  
+  c.innerHTML = (a * b);
+}
+function divide(a, b) {
+  c = document.getElementById("para1");
+  
+  c.innerHTML = (a / b);
 }
